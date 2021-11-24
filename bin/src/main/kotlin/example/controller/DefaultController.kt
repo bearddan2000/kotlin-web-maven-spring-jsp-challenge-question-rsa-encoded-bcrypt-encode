e58d1b7fa7aec7e6597d68af2fcd480422b7f636
bearddan2000 <bearddan2000@yahoo.com>
2021-11-24 10:44:52 -0500

    initial commit

---
+++ bin/src/main/kotlin/example/controller/DefaultController.kt
@@ -0,0 +1,77 @@
+package example.controller
+
+import example.service.ChallengeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Controller
+class DefaultController {
+
+  @Autowired
+  private lateinit var challengeService: ChallengeService;
+
+  @GetMapping("/")
+  fun index(): String {
+      return "/home"
+  }
+
+  @GetMapping("/home")
+  fun home(): String {
+      return "/home"
+  }
+
+  @GetMapping("/admin")
+  fun admin(): String {
+      return "/admin"
+  }
+
+  @GetMapping("/super")
+  fun super1(): String {
+      return "/super"
+  }
+
+  @GetMapping("/user")
+  fun user(): String {
+      return "/user"
+  }
+
+  @GetMapping("/about")
+  fun about(): String {
+      return "/about"
+  }
+
+  @GetMapping("/login")
+  fun login(): String  {
+      return "/login"
+  }
+
+  @GetMapping("/logout")
+  fun logout(): String {
+    SecurityContextHolder.getContext().setAuthentication(null);
+    return "/login"
+  }
+
+  @GetMapping("/403")
+  fun error403(): String {
+      return "/error/403"
+  }
+
+	@GetMapping("/register")
+	fun register(model: Model): String
+	{
+    model.addAttribute("challengeQuestionList", challengeService.findAllQuestions());
+		return "/register";
+	}
+
+	@GetMapping("/reset")
+	fun reset(model: Model): String
+	{
+    model.addAttribute("challengeQuestionList", challengeService.findAllQuestions());
+		return "/reset";
+	}
+}
